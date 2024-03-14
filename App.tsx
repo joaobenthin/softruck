a8168eeb34c12b4a8fc38793ccb9b4212bc5d53b
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-03-14 11:27:17 -0300

    feat: add theme on project
    Add theme on project using styled-components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
-import { Home } from './src/screens/Home'
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+
+import { Routes } from './src/routes'
+import theme from './src/theme'
 
 export function App() {
-  return <Home />
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </ThemeProvider>
+  )
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,28 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    BRAND_LIGHT: '#00B37E',
+    BRAND_MID: '#00875F',
+
+    BLUE_100: '#dbeafe',
+    BLUE_200: '#c7d2fe',
+    BLUE_300: '#a5b4fc',
+    BLUE_400: '#818cf8',
+    BLUE_500: '#6366f1',
+    BLUE_600: '#2563eb',
+    BLUE_700: '#1d4ed8',
+    BLUE_800: '#1e40af',
+    BLUE_900: '#1e3a8a',
+  },
+
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+    XXL: 24,
+    XXXL: 32,
+  },
+}
